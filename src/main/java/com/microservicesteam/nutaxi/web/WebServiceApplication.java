47784e9b66d89993efedfd3ad688af4aee7a2cb0
Andor Kelemen <andor.kelemen@gmail.com>
2016-09-07 21:39:57 +0200

    Initial Application

---
+++ src/main/java/com/microservicesteam/nutaxi/web/WebServiceApplication.java
@@ -0,0 +1,29 @@
+package com.microservicesteam.nutaxi.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import com.netflix.appinfo.AmazonInfo;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class WebServiceApplication {
+
+    @Bean
+    @Profile("docker-aws")
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+        AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+        config.setDataCenterInfo(info);
+        return config;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceApplication.class, args);
+    }
+}
