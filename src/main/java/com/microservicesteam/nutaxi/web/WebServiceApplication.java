64c1d3e00f9a258456ebbbb9b1edfa55d4a661f1
Andor Kelemen <andor.kelemen@gmail.com>
2016-09-14 20:49:45 +0200

    Wire Zuul

--- src/main/java/com/microservicesteam/nutaxi/web/WebServiceApplication.java
+++ src/main/java/com/microservicesteam/nutaxi/web/WebServiceApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
 import com.netflix.appinfo.AmazonInfo;
 
+@EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
 public class WebServiceApplication {
